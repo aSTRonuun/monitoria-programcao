023e3371d3d8dcda1f1e1bd038aae0a2fc154a64
aSTRonunn <vitormanoel00710@gmail.com>
2021-05-07 22:06:49 -0300

    🚀 update: fundamentos c/c++

---
+++ fundamentos-c-c++/estrutura-basica/estrutura-basica.cpp
@@ -0,0 +1,23 @@
+
+/*
+-------------- C ----------------
+#include <stdio.h>
+
+int main(void){
+    puts("Ola mundo do C!");
+
+    return 0;
+}
+*/
+
+//-------------- C++ -------------
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Ola mundo do C++!" << endl;
+
+    return 0;
+}
+
---
+++ fundamentos-c-c++/tipos-de-dados/tipos-de-dados.cpp
@@ -0,0 +1,44 @@
+/*
+-------------- C -------------
+#include <stdio.h>
+
+int main(){
+
+    char nome[30] = "Victor";
+
+    printf("O nome eh %s", nome);
+
+    return 0;
+}
+*/
+
+
+
+
+
+//-------------- C++ -------------
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    int x = 10;
+    float pi = 3.1415;
+    double luz = 299.792458;
+    char letra = 'a';
+    string nome = "Victor";
+
+    cout << "O numero eh " << x << endl;
+
+    cout << "O valor de pi aproximado eh " << pi << endl;
+
+    cout << "A velocidade da luz eh " << luz << endl;
+
+    cout << "A letra eh " << letra << endl;
+
+    cout << "O nome eh " << nome << endl;  
+
+    return 0;
+}
